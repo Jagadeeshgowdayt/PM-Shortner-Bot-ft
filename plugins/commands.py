31a716286517005a1acd1129c811e0e2033cd983
Jagadeeshgowdayt <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2023-08-26 16:55:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,9 +22,7 @@ BATCH_FILES = {}
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-         buttons = [[
-                          InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")
-        ]]
+        buttons = [[InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
@@ -37,9 +35,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
-                          InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")
-        ]]
+        buttons = [[InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
@@ -77,9 +73,7 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-         buttons = [[
-                          InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")
-        ]]
+        buttons = [[InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")]
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
             photo=random.choice(PICS),
