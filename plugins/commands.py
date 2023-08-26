b61dfbe5418bb039e25d47d617ed4c25bae6564e
Jagadeeshgowdayt <98593308+Jagadeeshgowdayt@users.noreply.github.com>
2023-08-26 16:56:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -73,7 +73,7 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-        buttons = [[InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")]
+        buttons = [[InlineKeyboardButton("send movie name ", url="https://t.me/kannadahdall")]]
         reply_markup = InlineKeyboardMarkup(buttons)      
         await message.reply_photo(
             photo=random.choice(PICS),
